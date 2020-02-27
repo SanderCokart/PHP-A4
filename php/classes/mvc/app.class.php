eeb379e451145ed346c892d72255982a775b2d80
Sander Cokart <cokart32@gmail.com>
2020-02-27 08:51:21 +0100

    dag 1

---
+++ php/classes/mvc/app.class.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mvc;
+
+class App
+{
+    public function __toString()
+    {
+        return 'this is app';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,18 +1,17 @@
-<?php require_once("../php/includes/default.inc.php") ?>
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <title>MVC structuur</title>
-     <link rel="stylesheet" href="css/master.css">
-   </head>
-   <body style="">
-     <?php
-        // code...
+<?php use mvc\App;
 
-        //example
-        $test = new Test();
-        echo $test;
-      ?>
-   </body>
- </html>
+require_once("../php/includes/default.inc.php") ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>MVC structuur</title>
+    <link rel="stylesheet" href="css/master.css">
+</head>
+<body style="">
+<?php
+$app = new App();
+echo $app;
+?>
+</body>
+</html>
